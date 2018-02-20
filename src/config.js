2512c16a425847eaad492b28c846f80b6f6a1de3
dhiraj161298 <sumankumaribarnwal@gmail.com>
2018-02-20 14:25:04 +0530

    API url fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const ACL_API="https://acl-server.herokuapp.com/api/"
-// export const ACL_API="http://10.102.45.41:4000/api/"
-export const ACL_API="http://localhost:4000/api/"
+// export const ACL_API="http://localhost:4000/api/"
+export const ACL_API="http://10.5.20.214/aclapi/api/"
