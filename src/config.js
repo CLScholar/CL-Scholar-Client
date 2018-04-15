7ef9ec02a1be08e8d4c1a26fdd0871dec7df1e66
Dhiraj Barnwal <sumankumaribarnwal@gmail.com>
2018-04-15 13:18:34 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const ACL_API="http://localhost:4000/api/"
-// export const ACL_API="http://cnerg.iitkgp.ac.in/aclapi/api/"
-export const ACL_API=`http://${window.location.hostname}/aclapi/api/`
+export const ACL_API="http://cnerg.iitkgp.ac.in/aclapi/api/"
+// export const ACL_API=`http://${window.location.hostname}/aclapi/api/`
